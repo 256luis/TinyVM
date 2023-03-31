6744c039dc0600118f4cc1dced290a096e38fb5d
luisbanez <luismbanez@gmail.com>
2023-04-01 00:45:47 +0800

    add comment about main.c file

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+// this file is currently being used for testing purposes
+
 #include <stdio.h>
 #include "vm.h"
 #include "types.h"
@@ -20,6 +22,13 @@ int main()
     vm_execute_instruction(vm, SUBI, DWORD_ZERO);
     vm_execute_instruction(vm, PUSHL, dword_int(2));
     vm_execute_instruction(vm, SUBI, DWORD_ZERO);    
+    vm_execute_instruction(vm, PUSHL, dword_int(0xFFFFFFFF));
+    vm_execute_instruction(vm, PUSHL, dword_int(0xFF00F0FF));
+    vm_execute_instruction(vm, AND, DWORD_ZERO);
+    vm_execute_instruction(vm, AND, DWORD_ZERO);
+    vm_execute_instruction(vm, PUSHL, dword_int(0xFF000000));
+    vm_execute_instruction(vm, PUSHL, dword_int(0x00FF0000));
+    vm_execute_instruction(vm, OR, DWORD_ZERO);    
     
     vm_dump(vm);
 
