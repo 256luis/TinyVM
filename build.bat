d623568ad1723efab5ded5bc6c9624701b4bc56b
luisbanez <luismbanez@gmail.com>
2023-04-24 22:20:37 +0800

    build and check script changes

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@
 set CC=clang
 set CFLAGS=-fsanitize=address -g -O0 -Iinclude -std=c17 -Wall -Wextra -Wpedantic -pedantic -Wno-strict-prototypes -Wmissing-prototypes -Wimplicit-fallthrough
 
-%CC% src/*.c -o build/main.exe %CFLAGS%
+%CC% src/*.c -o build/tvm.exe %CFLAGS%
--- check.bat
+++ check.bat
@@ -2,5 +2,5 @@
 
 set CFLAGS=-fsyntax-only -O3 -Iinclude -std=c17 -Wall -Wextra -Wpedantic -pedantic -Wno-strict-prototypes -Wmissing-prototypes -Wimplicit-fallthrough
 
-gcc src/*.c -o build/main.exe %CFLAGS%
-clang src/*.c -o build/main.exe %CFLAGS%
+gcc src/*.c %CFLAGS%
+clang src/*.c %CFLAGS%
